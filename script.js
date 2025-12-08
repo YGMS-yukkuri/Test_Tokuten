b9b46637aab469538595a6b19ed27799b9ba8fb5
yukkuri <reityan.sakura0104@gmail.com>
2025-12-08 22:27:06 +0900

    列に要素を追加可能に

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const subjectBox = document.getElementById("subject");
 const scoreBox = document.getElementById("score");
 const avgscoreBox = document.getElementById("avgscore");
 
+const tbody = document.querySelector(".tbody");
+
 let subject;
 let score;
 let avgscore;
@@ -12,9 +14,51 @@ function register() {
     score = scoreBox.value;
     avgscore = avgscoreBox.value;
 
+    if(subject.length >= 10){
+        alert("教科名が長すぎます");
+        subjectBox.value = "";
+        return;
+    }
+
     score = Number(score);
     avgscore = Number(avgscore);
     gapscore = score - avgscore;
-    
-    
+
+    const tr = document.createElement("tr");
+    tr.classList.add("data");
+
+    const th = document.createElement("th");
+    th.scope = "row";
+    th.classList.add("rows")
+    th.textContent = subject;
+
+    const tdScore = document.createElement("td");
+    tdScore.textContent = String(score);
+
+    const tdAverage = document.createElement("td");
+    tdAverage.textContent = String(avgscore);
+
+    const tdGap = document.createElement("td");
+    if(gapscore > 0){
+        gapscore = `+${String(gapscore)}`
+    }
+    tdGap.textContent = gapscore;
+
+    const tdTime = document.createElement("td");
+    const nowTime = new Date();
+    const nowJST = nowTime.toLocaleString("ja-jp", {
+        timeZone: "Asia/Tokyo",
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit"
+    })
+    tdTime.textContent = nowJST;
+
+    tr.appendChild(th);
+    tr.appendChild(tdScore);
+    tr.appendChild(tdAverage);
+    tr.appendChild(tdGap);
+    tr.appendChild(tdTime);
+
+    tbody.appendChild(tr);
 }
