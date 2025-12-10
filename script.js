76eaedeffa92b245451a67b54e02122f85c49d09
yukkuri <reityan.sakura0104@gmail.com>
2025-12-10 18:24:32 +0900

    fix: 編集画面の教科入力欄の初期値を「言語文化」に設定し、変数名を修正

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const subjectBox = document.getElementById("subject");
 const scoreBox = document.getElementById("score");
 const avgscoreBox = document.getElementById("avgscore");
-const editSubjectBox = document.getElementById("editSubject");
+const SearchSubjectBox = document.getElementById("editSubject");
 
 const tbody = document.querySelector(".tbody");
 
@@ -84,7 +84,8 @@ function search() {
     console.log(allSubject);
 
     allSubject.forEach((element, index) => {
-        if (element.textContent == editSubjectBox.value) {
+        if (element.textContent == SearchSubjectBox.value) {
+            const prevSubject = allSubject[index].textContent;
             const prevScore = Number(allScore[index].textContent);
             const prevAvg = Number(allAvg[index].textContent);
             const prevGap = Number(allGap[index].textContent);
@@ -95,18 +96,36 @@ function search() {
             EditBox.style.flexDirection = "column";
             EditBox.style.justifyContent = "center";
             EditBox.style.alignItems = "center";
-            EditBox.style.width = "80dvw";
-            EditBox.style.height = "50dvh";
+            EditBox.style.width = "100dvw";
+            EditBox.style.height = "100dvh";
             EditBox.style.transform = "translate(-50%,-50%)";
             EditBox.style.position = "fixed";
             EditBox.style.top = "50%";
             EditBox.style.left = "50%";
+            EditBox.style.backgroundColor = "#00000040"
 
             const EditDiv = document.createElement("div");
-
+            EditDiv.style.display = "flex"
+            EditDiv.style.width = "50%"
+            EditDiv.style.height = "50%"
+            EditDiv.style.flexDirection = "column";
+            EditDiv.style.justifyContent = "center";
+            EditDiv.style.alignItems = "center";
+            EditDiv.style.backgroundColor = "rgba(255, 210, 210, 1)"
+            EditDiv.style.border = "1px solid #000"
             
+
+            const EditSubjectLabel = document.createElement("label");
+            EditSubjectLabel.textContent = `教科を入力(編集前:${prevSubject})`;
+            EditSubjectLabel.htmlFor = "EditSubjectInput";
+
+            const EditSubject = document.createElement("input");
+            EditSubject.type = "text";
+            EditSubject.id = "EditSubjectInput";
+            EditSubject.defaultValue = prevSubject;
+
             const EditScoreLabel = document.createElement("label");
-            EditScoreLabel.textContent = "得点を入力";
+            EditScoreLabel.textContent = `得点を入力(編集前:${prevScore})`;
             EditScoreLabel.htmlFor = "EditScoreInput";
 
             const EditScore = document.createElement("input");
@@ -115,7 +134,7 @@ function search() {
             EditScore.defaultValue = prevScore;
 
             const EditAvgLabel = document.createElement("label");
-            EditAvgLabel.textContent = "平均点を入力";
+            EditAvgLabel.textContent = `平均点を入力(編集前:${prevAvg})`;
             EditAvgLabel.htmlFor = "EditAvgInput";
 
             const EditAvg = document.createElement("input");
@@ -123,12 +142,26 @@ function search() {
             EditAvg.id = "EditAvgInput";
             EditAvg.defaultValue = prevAvg;
 
+            const SubmitButton = document.createElement("button");
+            SubmitButton.onclick = () => Editing();
+            SubmitButton.textContent = "修正";
+
+            EditDiv.appendChild(EditSubjectLabel);
+            EditDiv.appendChild(EditSubject);
             EditDiv.appendChild(EditScoreLabel);
             EditDiv.appendChild(EditScore);
             EditDiv.appendChild(EditAvgLabel);
             EditDiv.appendChild(EditAvg);
+            EditDiv.appendChild(SubmitButton);
+
+            EditBox.appendChild(EditDiv);
+            document.querySelector("main").appendChild(EditBox);
         }
     })
 
 
 }
+
+function Editing(){
+    console.log("OK");
+};
